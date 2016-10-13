16d69f12b938ca2eea2d13d24ebeb7e1e9b7fc06
Station0391 <Station0391@Station039107.Dlink>
2016-10-13 21:35:13 +0300

    more expression added

--- HomeWorks/src/lesson160329/Task01.java
+++ HomeWorks/src/lesson160329/Task01.java
@@ -4,7 +4,7 @@ public class Task01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Hello, world!".toUpperCase());
+		System.out.println("Hello, world!".toUpperCase());
 		System.out.println(1+2+3+4+5+6+7+8+9+10);
 
 	}
