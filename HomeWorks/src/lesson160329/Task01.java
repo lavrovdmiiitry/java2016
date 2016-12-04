cdad1d28eed8d4e5278a57799f273a106bdaab74
Station0391 <Station0391@Station039107.Dlink>
2016-12-04 17:15:28 +0300

    hello mom added

--- HomeWorks/src/lesson160329/Task01.java
+++ HomeWorks/src/lesson160329/Task01.java
@@ -5,7 +5,8 @@ public class Task01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello, world!!!!".toUpperCase());
-		System.out.println(1+2+3+4+5+6+7+8+9+10);
+		System.out.println("Hello, Mom!!!!".toUpperCase());
+		;
 
 	}
 
