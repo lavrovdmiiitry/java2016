a88d48269ee7582881841582ae80b263626c206a
Station0391 <Station0391@Station039107.Dlink>
2016-12-04 17:16:28 +0300

    removed extra exclamation marks

--- HomeWorks/src/lesson160329/Task01.java
+++ HomeWorks/src/lesson160329/Task01.java
@@ -4,9 +4,9 @@ public class Task01 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello, world!!!!".toUpperCase());
-		System.out.println("Hello, Mom!!!!".toUpperCase());
-		System.out.println("Hello, Dad!!!!".toUpperCase());
+		System.out.println("Hello, world!".toUpperCase());
+		System.out.println("Hello, Mom!".toUpperCase());
+		System.out.println("Hello, Dad!".toUpperCase());
 		;
 
 	}
