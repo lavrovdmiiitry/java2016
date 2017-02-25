5342bf792b2d95ec000a0cca91fc03c1f96d51c9
Station0391 <Station0391@Station039107.Dlink>
2017-02-25 19:11:36 +0300

    lesson 160331

---
+++ HomeWorks/src/lesson160331/Homework.java
@@ -0,0 +1,15 @@
+package lesson160331;
+
+public class Homework {
+
+	public static void main(String[] args) {
+	
+		int x = 50_000;
+		double d = 0.13;
+		
+		double z = x * (1 - d) ;
+		System.out.println(z);		
+
+	}
+
+}
